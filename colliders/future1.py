fa6e938348aee17a77e17b1baa6e165d4a238d24
Kevin Pedro <kpedro88@gmail.com>
2023-04-03 13:42:53 -0500

    minor updates to future scenarios

--- colliders/future1.py
+++ colliders/future1.py
@@ -1,7 +1,7 @@
 from current import colliders
 
 colliders.extend([
-    {"name": "FCC-ee (1)", "start": 2048, "end": 2057, "energy": 240, "unit": "GeV", "particles": ["e"]},
+    {"name": "FCC-ee (1)", "start": 2048, "end": 2057, "energy": 250, "unit": "GeV", "particles": ["e"]},
     {"name": "FCC-ee (2)", "start": 2059, "end": 2063, "energy": 350, "unit": "GeV", "particles": ["e"]},
-    {"name": "FCC-hh", "start": 2074, "end": 2099, "energy": 100, "unit": "TeV", "particles": ["p"]},
+    {"name": "FCC-hh", "start": 2073, "end": 2099, "energy": 100, "unit": "TeV", "particles": ["p"]},
 ])
